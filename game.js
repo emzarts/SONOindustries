e8f398143ee6c1c717ffa0fec4d0827bd5e8f70a
Emily <17zimmermane@gmail.com>
2017-07-12 17:56:46 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+var game = false;
 var showText = function(target, message, index, interval) {
     var result = " ";
     var i = 0;
@@ -5,7 +6,12 @@ var showText = function(target, message, index, interval) {
   	result = message.substring(index, index + 1);
     document.getElementById(target).append(result);
   	setTimeout(function() {showText(target, message.substring(index - message.length), index + 1, interval);}, interval);
+        
     }
+    if (game === true && message.length <= i) {
+            input();
+    }
+    
 }
 
 function intro() {
@@ -15,13 +21,26 @@ function intro() {
 }
 
 function startGame() {
+    game = true;
     document.getElementById("content1").style.opacity = "0";
     document.getElementById("content2").style.opacity = "1";
     setTimeout(function() {document.getElementById("content2").style.width = "100%";},1900);
     setTimeout(function() {document.getElementById("content1").style.width = "0";}, 1900);
     setTimeout(function() {showText("text", "You wake up. It is cold and dark. There is a computer in the corner. Its screen flickers gently, creating a soft glow. *ERROR* You crawl towards the light, and notice numbers on the screen. They are the same numbers scrawn hastily on the collar of your clothes. You realize it is a log made up of numerous entries dating back for months. The most recent entry is dated \"June 8th, 2017\" you must have been sleeping for a very long time.", 0, 60);}, 3000);
-    setTimeout(function() {input();}, 29800);
 
+}
+
+// add line break before the input
+
+function input() {
+    document.getElementById("text").appendChild(document.createElement("br"));
+    document.getElementById("text").append("> ");
+    var x = document.createElement("INPUT");
+    x.setAttribute("id", "input-box");
+    x.setAttribute("type", "text");
+    x.setAttribute("autofocus", "true");
+    x.setAttribute("onkeydown", "handleKeyPress(this)");
+    document.getElementById("text").append(x);
 }
 
 function handleKeyPress(e){
@@ -29,16 +48,21 @@ function handleKeyPress(e){
     var key=event.keyCode || e.which;
     if (key==13){
         alert("you typed: " + text);
-  }
+        keyPressEvent(text);
+    }
 }
 
-function input() {
-    document.getElementById("content2").append("> ");
-    var x = document.createElement("INPUT");
-    x.setAttribute("type", "text");
-    x.setAttribute("autofocus", "true");
-    x.setAttribute("onkeydown", "handleKeyPress(this)");
-    document.getElementById("content2").append(x);
+function keyPressEvent(input) {
+    document.getElementById("text").append(" " + input);
+    document.getElementById("input-box").style.display = "none";
+    document.getElementById("text").appendChild(document.createElement("br"));
+    if (input.toUpperCase() === "help".toUpperCase()) {
+        showText("text","here is help",0,60);
+    }
+    else {
+        
+    }
+    input();
 }
 
 // make intro text with JS
